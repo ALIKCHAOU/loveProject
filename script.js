1cefd50bda186f28ca371a7da4badf4f9abf2a3f
ali kchaou <alikchaoufss2@gmail.com>
2018-08-18 01:04:15 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getRandomPosition(element) {
+	/*var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;*/
+	var randomX = Math.floor(Math.random()*700);
+	var randomY = Math.floor(Math.random()*1100);
+	
+ 
+  return [randomX,randomY];
+}
+ window.onload = async function() {
+	for(let i=0;i<50;i++)
+  {var img = document.createElement('img');
+	
+	img.setAttribute("src", "http://www.emoji.co.uk/files/twitter-emojis/smileys-people-twitter/10497-smiling-face-with-heart-shaped-eyes.png");
+   	img.setAttribute("id", "img");
+	document.body.appendChild(img);
+	var xy = getRandomPosition(img);
+	
+	img.style.top = xy[0] + 'px';
+	img.style.left = xy[1] + 'px';
+    await sleep( Math.floor(Math.random()*2000));
+  var img = document.createElement('img');
+	
+	img.setAttribute("src", "https://cdn.iconscout.com/public/images/icon/free/png-512/love-heart-social-media-facebook-emoticons-reactions-35be565224846ce6-512x512.png");
+   img.setAttribute("id", "img");
+   
+	document.body.appendChild(img);
+	var xy = getRandomPosition(img);
+	img.style.top = xy[0] + 'px';
+	img.style.left = xy[1] + 'px';
+    await sleep( Math.floor(Math.random()*2000));
+  var img = document.createElement('img');
+	
+	img.setAttribute("src", "https://www.viidakkorumpu.fi/wp-content/uploads/2015/02/syd%C3%A4n.png");
+   img.setAttribute("id", "img");
+	document.body.appendChild(img);
+	var xy = getRandomPosition(img);
+	img.style.top = xy[0] + 'px';
+	img.style.left = xy[1] + 'px';}
+  await sleep( Math.floor(Math.random()*2000));
+
+
+}
